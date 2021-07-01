7fde0f1f9a63b9d4082129084c06bb32e67a7e7c
josh-dughi-snapdocs <82908437+josh-dughi-snapdocs@users.noreply.github.com>
2021-07-01 17:35:59 -0400

    tests

--- test/controllers/properties_controller_test.rb
+++ test/controllers/properties_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class PropertiesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @property = properties(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
